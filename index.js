c8a3c630dd8332fcf467eec0c6a2db38638cb05e
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-08 10:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
     const dodgerRightEdge = dodgerLeftEdge+40
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    
+
 
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge+20
